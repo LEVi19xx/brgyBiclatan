c9865aa5d03cd399a632b0f9dcbeccce555e985d
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-05 20:49:17 +0800

    sdfs

--- index_login.php
+++ index_login.php
@@ -391,7 +391,7 @@ a:hover{
                    <div class="div">
                         <h5>Password</h5>
                         <input type="password" class="input" id="password" name="password">
-                        <span class="eye-icon" onclick="togglePasswordVisibility()">👁️</span>
+                        <span class="eye-icon" onclick="togglePasswordVisibility()">https://icons8.com/icon/60022/eye</span>
                    </div>
                 </div>
                 <div class="div">
