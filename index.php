2cc5ca1303605ec6f9523c1f85e0069d06f9c557
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-06 01:14:59 +0800

    new

--- index.php
+++ index.php
@@ -1275,7 +1275,7 @@ nav {
     require('classes/conn.php');
 
     // Assuming $id_services contains the ID of the service
-    $id_services = 6; // Example service ID
+    $id_services = 4; // Example service ID
 
     // Query the database to fetch the filename of the image associated with the service ID
     $sql = "SELECT image_service FROM tbl_services WHERE id_services = :id";
@@ -1345,7 +1345,7 @@ nav {
             require('classes/conn.php');
 
             // Assuming $id_services contains the ID of the service
-            $id_services = 5; // Example service ID
+            $id_services = 3; // Example service ID
 
             // Query the database to fetch the filename of the image associated with the service ID
             $sql = "SELECT image_service FROM tbl_services WHERE id_services = :id";
