1f2fb91f2c4f16465990647c23f5267bae5fbdd7
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-09 14:04:28 +0800

    new

--- dashboard_sidebar_end.php
+++ dashboard_sidebar_end.php
@@ -15,45 +15,6 @@
 
 <body id="page-top">
 
-    <!-- Page Wrapper -->
-    <div id="wrapper" class="d-flex flex-column" style="min-height: 100vh;">
-
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
-            <div id="content">
-
-                <!-- Scroll to Top Button-->
-                <a class="scroll-to-top rounded" href="#page-top">
-                    <i class="fas fa-angle-up"></i>
-                </a>
-
-                <!-- Logout Modal-->
-                <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                    aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">×</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                            <div class="modal-footer">
-                                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                                <a class="btn btn-primary" href="logout.php">Logout</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <!-- End of Content -->
-
-        </div>
-        <!-- End of Content Wrapper -->
 
         <!-- Footer -->
         <footer id="footer" class="d-flex-column text-center" style="background-color: #007bff; color: white; width: 100%;">
